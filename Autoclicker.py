96f8678ba76abe266d51f5446165546596d6a838
Arshadul-Monir <arshadulmonir@gmail.com>
2024-01-28 19:31:31 -0500

    Remade clicker with threading

--- Autoclicker.py
+++ Autoclicker.py
@@ -1,37 +1,44 @@
-import pygame
+import tkinter as tk
 from pynput import keyboard
 from pynput.mouse import Button, Controller
 import time
+import threading
 
-# Initialize GUI
-screen = pygame.display.set_mode((500, 500))
+class GUI:
+    def __init__(self):
+        # Instance variables
+        self.playing = False
+        # self.listen_thread = None
+        
+        root = tk.Tk()
+        root.geometry(f'{400}x{400}+{(root.winfo_screenwidth() - 400) // 2}+{(root.winfo_screenheight() - 400) // 2}')
+        
+        keyboard_listener = keyboard.Listener(on_press = self.toggle)
+        keyboard_listener.start()
+        root.mainloop()
 
-# Global toggler variable
-playing = False
-
-# Function to compare keypress with hotkey and toggles if matches
-def toggle(key):
-    global playing
-    if key == keyboard.Key.space:
-        playing = not playing
-
-# Starts key listener
-keyboard_listener = keyboard.Listener(on_press=toggle)
-keyboard_listener.start()
-
-# Main loop
-while True:
-    # Closing program
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
-    # Continues clicking while global variable is set to true
-    if playing:
-        Controller().click(Button.left)
-        time.sleep(1)
-        print("Clicked")
     
-    # Updates display
-    pygame.display.flip()
+    def click(self):
+        # Continues clicking while global variable is set to true
+        while self.playing:
+            Controller().click(Button.left)
+            print("Clicked")
+            time.sleep(1/100)
+ 
+            
+
+    # Function to compare keypress with hotkey and toggles if matches
+    def toggle(self,key):
+        print(key)
+        if key == keyboard.Key.space:
+            self.playing = not self.playing 
+            listen_thread = threading.Thread(target=self.click)
+            listen_thread.start()
+            
+    
+    
+    def __del__(self):
+        keyboard.Listener.stop()
+        
+        
+clicker = GUI()
