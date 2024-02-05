dd2baed501a400601752495f039a25346f0220a1
Arshadul-Monir <arshadulmonir@gmail.com>
2024-02-05 12:43:06 -0500

    Limited CPS

--- Autoclicker.py
+++ Autoclicker.py
@@ -14,24 +14,24 @@ keybinds = {
     "decrement_speed": keyboard.Key.down,   # to be implemented
 }
 current_binding_key = None
+hotkey_listener = None
 
+# Starts a new keyboard Listener in the choose_key function
 def bind(keybind):
     global current_binding_key, hotkey_listener
     current_binding_key = keybind
-    keyboard_listener.stop()
     hotkey_listener = keyboard.Listener(on_press=choose_key)
-    print("before start")
-    hotkey_listener.start()
-    print("after start")
-    
+    hotkey_listener.start()    
 
- 
+# Takes listened key 
 def choose_key(key):
-    print("you stupid")
-    global current_binding_key, hotkey_listener
-    keybinds[current_binding_key] = key
-    current_binding_key = None
-    hotkey_listener.stop()
+    print("you pressed", key)
+    global current_binding_key
+    if current_binding_key:
+        keybinds[current_binding_key] = key
+        current_binding_key = None
+        hotkey_listener.stop()
+        print("Binding complete.")
     
     
 
@@ -51,18 +51,17 @@ def on_close():
     keyboard_listener.stop()  
     root.destroy()
 
-
 # Sets clicking speed when submit button is pressed
 def submit_speed():
     global cps
-    try:
-        cps = int(speed.get())
-    except ValueError:
-        print("Invalid input. Please enter a valid number for CPS.")
+    if int(speed.get()) == 0 or int(speed.get()) > 100:
+        print("Invalid input. Please enter a valid number for CPS")
+        return
+    cps = int(speed.get())
 
 
 # Function to compare keypress with hotkey and toggles if matches
-def toggle(key):
+def toggle(key):    
     global playing
     print(key, not playing)
     if key == keyboard.Key.space:
@@ -94,9 +93,14 @@ speed_button = tk.Button(root, text = "Submit CPS", command = submit_speed)
 speed_button.pack()
 root.protocol("WM_DELETE_WINDOW", on_close)
 
-binding_button = tk.Button(root, text="Bind Hotkey", command=lambda: bind("pause_unpause"))
-binding_button.pack()
+# No work yet
+# binding_button = tk.Button(root, text="Bind Hotkey", command=lambda: bind("pause_unpause"))
+# binding_button.pack()
+
+# position_button = tk.Button(root, text="Choose Position", command=click_position)
+# position_button.pack()
+
 keyboard_listener = keyboard.Listener(on_press = toggle)
 keyboard_listener.start()
-root.mainloop()
 
+tk.mainloop()
